56caa4fce731929522bf0a2ba2250900f81efb4e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-06 12:39:30 -0700

    square and cube finished

--- arithmetic.py
+++ arithmetic.py
@@ -22,11 +22,11 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of the input."""
-
+    return num1 * num1
 
 def cube(num1):
     """Return the cube of the input."""
-
+    return num1 * num1 * num1
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
